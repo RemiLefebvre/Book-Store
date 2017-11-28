7dd1cf5b54eda40ee668e0383427e36e093a52b5
remilefebvre <lefebvre.reminicolas@gmail.com>
2017-11-28 13:47:15 +0100

    add files for template

--- model/phpmyadmin.php
+++ model/phpmyadmin.php
@@ -1,3 +1,10 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=exoPOO', 'root', 'gesundheititit');
+try {
+  $db = new PDO('mysql:host=localhost;dbname=exoPOO', 'root', 'gesundheititit');
+}
+
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
  ?>
--- view/model/manager.php
+++
@@ -1,5 +0,0 @@
-<?php
-require("phpmyadmin.php");
-
-
- ?>
